409f0151f5c2e92b7246428cc2cd6236b6a2f6fe
DavoDev <davidlarrosa58@gmail.com>
2023-03-28 15:39:59 +0200

    structure and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Podcast from "./pages/Podcast"
+import Episode from "./pages/Episode"
 
 
 function App() {
 
 	return (
 		<div className="App">
-      App
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/podcast/:id" element={<Podcast />}/>
+      <Route path="/podcast/:id/episode/:episodeId" element={<Episode />}/>
+    </Routes>
+    </BrowserRouter>
 		</div>
   )
 }
